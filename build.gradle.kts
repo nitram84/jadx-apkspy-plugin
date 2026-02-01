1f885065afac4f77f0bebbd29386d3c40170ae59
nitram84 <nitram84@gmx.de>
2026-02-01 22:14:25 +0100

    chore: update gradle and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,42 +6,42 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 plugins {
 	`java-library`
 
-	id("com.gradleup.shadow") version "8.3.6"
+	id("com.gradleup.shadow") version "9.3.1"
 	id("com.diffplug.spotless") version "7.0.4"
 
 	// auto update dependencies with 'useLatestVersions' task
-	id("se.patrikerdes.use-latest-versions") version "0.2.18"
+	id("se.patrikerdes.use-latest-versions") version "0.2.19"
 	id("com.github.ben-manes.versions") version "0.53.0"
 }
 
 dependencies {
 	// use compile only scope to exclude jadx-core and its dependencies from result jar
-	compileOnly("io.github.skylot:jadx-cli:1.5.2")
-	compileOnly("io.github.skylot:jadx-core:1.5.2")
-	compileOnly("io.github.skylot:jadx-gui:1.5.2")
+	compileOnly("io.github.skylot:jadx-cli:1.5.3")
+	compileOnly("io.github.skylot:jadx-core:1.5.3")
+	compileOnly("io.github.skylot:jadx-gui:1.5.3")
 
 	// use same versions as jadx-gui
 	compileOnly("com.fifesoft:rsyntaxtextarea:3.6.0")
-	compileOnly("org.apache.commons:commons-lang3:3.17.0")
-	compileOnly("commons-io:commons-io:2.19.0")
-	compileOnly("ch.qos.logback:logback-classic:1.5.18")
+	compileOnly("org.apache.commons:commons-lang3:3.20.0")
+	compileOnly("commons-io:commons-io:2.21.0")
+	compileOnly("ch.qos.logback:logback-classic:1.5.22")
 
 	// use same versions as in jadx-java-convert
-	compileOnly("org.ow2.asm:asm:9.8")
-	implementation("org.ow2.asm:asm-tree:9.8")
+	compileOnly("org.ow2.asm:asm:9.9.1")
+	implementation("org.ow2.asm:asm-tree:9.9.1")
 
-	implementation("de.femtopedia.dex2jar:dex-tools:2.4.28")
+	implementation("de.femtopedia.dex2jar:dex-tools:2.4.34")
 	implementation("com.github.javaparser:javaparser-core:3.26.4")
 
-	testImplementation("io.github.skylot:jadx-cli:1.5.2")
-	testImplementation("io.github.skylot:jadx-core:1.5.2")
+	testImplementation("io.github.skylot:jadx-cli:1.5.3")
+	testImplementation("io.github.skylot:jadx-core:1.5.3")
 
-	testImplementation("org.apache.commons:commons-lang3:3.17.0")
-	testImplementation("ch.qos.logback:logback-classic:1.5.18")
-	testImplementation("org.assertj:assertj-core:3.26.3")
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.1")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.13.1")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.13.1")
+	testImplementation("org.apache.commons:commons-lang3:3.20.0")
+	testImplementation("ch.qos.logback:logback-classic:1.5.22")
+	testImplementation("org.assertj:assertj-core:3.27.7")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.2")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.0.2")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.2")
 }
 
 allprojects {
@@ -77,13 +77,12 @@ fun FormatExtension.commonFormatOptions() {
 repositories {
 	mavenLocal()
 	mavenCentral()
-	maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/")
 	google()
 }
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_11
-	targetCompatibility = JavaVersion.VERSION_11
+	sourceCompatibility = JavaVersion.VERSION_17
+	targetCompatibility = JavaVersion.VERSION_17
 }
 
 version = System.getenv("VERSION") ?: "dev"
